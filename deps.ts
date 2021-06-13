1b80eb96c295233e97aca3175b8982e80d988088
Georg Zepf <private@zepfgeorg.com>
2021-06-13 14:01:10 +0200

    chore: bump std version to 0.98.0

--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@
 
 export { makeExecutableSchema } from "https://deno.land/x/gql_tools@v7.0.5%2B4/lib/schema/mod.ts";
 export { graphql } from "https://cdn.skypack.dev/graphql@15.5.0?dts";
-export { error as logError, warning as logWarning } from "https://deno.land/std@0.97.0/log/mod.ts";
+export { error as logError, warning as logWarning } from "https://deno.land/std@0.98.0/log/mod.ts";
 
 export type { RouterMiddleware, RouterContext } from "https://deno.land/x/oak@v7.5.0/mod.ts";
 export type { IExecutableSchemaDefinition as ExecutableSchemaDefinition } from "https://deno.land/x/gql_tools@v7.0.5%2B4/lib/schema/mod.ts";
